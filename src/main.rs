2ba4ba6bfc03210442358b4b60dbaf13f18bf0ef
Serhii Plyhun <snuk188@gmail.com>
2018-05-28 11:17:16 +0200

    Get rid of prefixes

--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,24 @@ use plygui::*;
 use plygui_scintilla::*;
 
 fn main() {
-    let mut application = Application::with_name("Plygui test");
+    let mut application = plygui::imp::Application::with_name("Plygui test");
 
     let mut window = application.new_window("plygui!!", WindowStartSize::Exact(200, 200), WindowMenu::None);
 
     window.on_resize(Some(
-        (|_: &mut UiMember, w: u16, h: u16| {
+        (|_: &mut Member, w: u16, h: u16| {
              println!("win resized to {}/{}", w, h);
          }).into(),
     ));
 
-    let mut sc = Scintilla::new();
+    let mut sc = plygui_scintilla::imp::Scintilla::new();
     sc.set_layout_width(layout::Size::MatchParent);
     sc.set_layout_height(layout::Size::MatchParent);
     sc.set_layout_padding(layout::BoundarySize::AllTheSame(15).into());
     sc.on_resize(Some(
-        (|sc: &mut UiMember, w: u16, h: u16| {
+        (|sc: &mut Member, w: u16, h: u16| {
             println!("SCINTILLA HAS RESIZED to {}/{}", w, h);
-            let sc: &mut Scintilla = sc.as_any_mut().downcast_mut::<Scintilla>().unwrap();
+            let sc = sc.as_any_mut().downcast_mut::<plygui_scintilla::imp::Scintilla>().unwrap();
             sc.set_margin_width(0, 25);
          }).into(),
     ));
