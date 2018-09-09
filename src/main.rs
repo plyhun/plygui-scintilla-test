b73708abcecae3d2f3301c3fd798df7dfa895188
snuk182 <snuk188@gmail.com>
2018-09-09 11:29:33 +0200

    More complicated test

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,27 @@ extern crate plygui;
 extern crate plygui_scintilla;
 
 use plygui::*;
-use plygui_scintilla::*;
+use plygui_scintilla as scintilla;
 
-fn main() {
-    let mut application = plygui::imp::Application::with_name("Plygui test");
+fn create_frame(name: &str, child: Box<Control>) -> Box<Control> {
+	let mut frame = imp::Frame::with_label(name);    
+    frame.set_child(Some(child));
+    frame.into_control()
+}
 
-    let mut window = application.new_window("plygui!!", WindowStartSize::Exact(200, 200), WindowMenu::None);
+fn create_splitted(first: Box<Control>, second: Box<Control>) -> Box<Control> {
+	let mut splitted = imp::Splitted::with_content(first, second, layout::Orientation::Horizontal);
+	splitted.set_layout_width(layout::Size::MatchParent);
+    splitted.set_layout_height(layout::Size::WrapContent);
+    splitted.into_control()
+}
 
-    window.on_resize(Some(
-        (|_: &mut Member, w: u16, h: u16| {
-             println!("win resized to {}/{}", w, h);
-         }).into(),
-    ));
-
-    let mut sc = plygui_scintilla::imp::Scintilla::new();
+fn create_scintilla(text: &str) -> Box<Control> {
+	use scintilla::{Scintilla, NewScintilla};
+	
+	let mut sc = scintilla::imp::Scintilla::with_content(text);
     sc.set_layout_width(layout::Size::MatchParent);
     sc.set_layout_height(layout::Size::MatchParent);
-    sc.set_layout_padding(layout::BoundarySize::AllTheSame(15).into());
     sc.on_resize(Some(
         (|sc: &mut Member, w: u16, h: u16| {
             println!("SCINTILLA HAS RESIZED to {}/{}", w, h);
@@ -26,11 +30,21 @@ fn main() {
             sc.set_margin_width(0, 25);
          }).into(),
     ));
-    
-    window.set_child(Some(sc.into_control()));
+    sc.into_control()
+}
 
-    //window.set_child(Some(button));
-    //window.set_child(Some(sc));
+fn root() -> Box<Control> {
+	create_splitted(create_frame("Left", create_scintilla("Hello")), create_frame("Right", create_scintilla("World")))
+}
 
+fn main() {
+    let mut application = plygui::imp::Application::with_name("Plygui test");
+    let mut window = application.new_window("plygui!!", WindowStartSize::Exact(1024, 768), WindowMenu::None);
+    window.on_resize(Some(
+        (|_: &mut Member, w: u16, h: u16| {
+             println!("win resized to {}/{}", w, h);
+         }).into(),
+    ));
+    window.set_child(Some(root()));	
     application.start();
 }
